2b475f7af3e54b9b5fee9961335898e646a07556
kga-1999 <142679732+kga-1999@users.noreply.github.com>
2025-06-22 02:19:35 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,21 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: '/palmentar-web-one/', // أضف هذا السطر أو عدله حسب اسم الريبو
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    // mode === 'development' &&
+    // componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
+
