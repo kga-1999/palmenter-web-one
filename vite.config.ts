bee42c4b5d5f05c9afd9c5f8ca7872239fe439da
kga-1999 <142679732+kga-1999@users.noreply.github.com>
2025-06-23 02:25:44 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // اجعلها منها هكذا للنشر على Vercel
+ base: process.env.VITE_BASE_PATH || "/palmenter-web-one",, // اجعلها منها هكذا للنشر على Vercel
   server: {
     host: "::",
     port: 8080,
